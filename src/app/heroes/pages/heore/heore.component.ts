0f11df736def97f403f4c895595e4b1069ca1d1d
luis vallejo <lvallejo@externos.bancokestado.cl>
2022-07-09 18:22:53 -0400

    Creacion de componenetes

---
+++ src/app/heroes/pages/heore/heore.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-heore',
+  templateUrl: './heore.component.html',
+  styles: [
+  ]
+})
+export class HeoreComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
